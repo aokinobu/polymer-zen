79772097965538d7209ce8693b9be8772c034eaf
Nobuyuki Aoki <aokinobu@gmail.com>
2019-04-30 20:21:08 +0000

    format and xp connection

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-import {PolymerElement, html} from '@polymer/polymer';
+import { PolymerElement, html } from '@polymer/polymer';
 import 'polymer-zen-progress/index.js';
 import 'polymer-zen-menu/index.js';
 
 class MyElement extends PolymerElement {
 
-  static get properties() { }
+  static get properties() {
+    return {
+      xp: {
+        notify: true,
+        type: Number,
+        value: 0
+      }
+    }
+
 
   static get template() {
     return html`
       <style> .xp { color: green; } </style>
       Welcome to Zen
-      <zen-progress></zen-progress>
-      <zen-menu></zen-menu>
+      <zen-progress xp=[[xp]]></zen-progress>
+      <zen-menu xp=[[xp]]></zen-menu>
     `;
   }
+
+
 }
 
 customElements.define('wc-zen', MyElement);
